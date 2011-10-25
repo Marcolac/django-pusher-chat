162f788cfde19f1bfd1cd4a935e1c795603c3482
Andres Torres <andres.torres.marroquin@gmail.com>
2011-10-24 23:00:08 -0500

    First commit

---
+++ pusher_chat/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'pusher_chat.views.home', name='home'),
+    # url(r'^pusher_chat/', include('pusher_chat.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
